b17b19d4d46d8f02b7b0e4dddd24c1d96ad6a83f
ouahdinaaaa <a.ouahdina@gmail.com>
2023-12-10 11:28:27 +0100

    push

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,12 @@
 /*   By: ayael-ou <ayael-ou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 16:55:11 by ayael-ou          #+#    #+#             */
-/*   Updated: 2023/12/09 23:36:00 by ayael-ou         ###   ########.fr       */
+/*   Updated: 2023/12/10 11:25:15 by ayael-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
-
 
 int main(void)
 {
@@ -30,30 +29,5 @@ int main(void)
     {
         std::cerr << e.what() << std::endl;
     }
-    
-    Form Solo("solo");
-    Form aya("aya", 3, 3);
-    Form benin("benin", 2, 20);
-    
-    std::cout << jonh << std::endl;
-    std::cout << Solo << std::endl;
-    std::cout << aya << std::endl;
-    std::cout << benin << std::endl;
-    jonh.signForm(&Solo);
-    jonh.signForm(&aya);
-    jonh.signForm(&benin);
-    
-    try
-    {
-        jonh.decrementGrade();
-        std::cout << jonh << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    jonh.signForm(&aya);
-    std::cout << Solo << std::endl;
-    std::cout << aya << std::endl;
-    std::cout << benin << std::endl;
+    // faire bureaucrat puis chacun faire une action
 }
